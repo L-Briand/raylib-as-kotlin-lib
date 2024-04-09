163cc843f8f7deae9ee030e228135848daeb036f
Lionel Briand <lionel.briand@orandja.fr>
2024-04-09 19:32:45 +0200

    wip

--- core/src/macosX64Main/kotlin/net/Main.kt
+++ core/src/macosX64Main/kotlin/net/Main.kt
@@ -1,30 +1,16 @@
 package net
 
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.cValue
+import kotlinx.cinterop.*
 import raylib.*
 
 @OptIn(ExperimentalForeignApi::class)
 fun main() {
 
     InitWindow(800, 450, "raylib [core] example - basic window");
-
-    val white = cValue<Color> {
-        r = 255u
-        g = 255u
-        b = 255u
-        a = 255u
-    }
-
-    val lightGray = cValue<Color> {
-        r = 255u
-        a = 255u
-    }
-
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(white);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, lightGray);
+        ClearBackground(white.readValue());
+        DrawText("Congrats! You created your first window!", 190, 200, 20, lightgray.readValue());
         EndDrawing();
     }
 
